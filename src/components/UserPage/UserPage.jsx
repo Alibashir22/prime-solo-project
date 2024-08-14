27fabfa6a1891dd2b5ab66241dd62fc22ad33940
Alibashir Ahmed <alibashirahmed26@gmail.com>
2024-08-14 12:18:28 -0500

    worked on css for workout history

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,37 +1,37 @@
 import React from 'react';
 
-import {useSelector} from 'react-redux';
+import { useSelector } from 'react-redux';
 import Workout from './workout';
 
 function UserPage() {
   const user = useSelector((store) => store.user);
-const sampleworkouts=[
-  {
-    workout_id:1,
-    workout_date:"2024-08-10",
-    notes:"this is my history page",
-    exercises:"Push-up, Squat"
-  },
-  {
-    workout_id:2,
-    workout_date:"2024-08-10",
-    notes:"this is my history page",
-    exercises:"Push-up, Squat"
-  },
-  {
-    workout_id:3,
-    workout_date:"2024-08-10",
-    notes:"this is my history page",
-    exercises:"Push-up, Squat"
-  }
-]
+  const sampleworkouts = [
+    {
+      workout_id: 1,
+      workout_date: "2024-08-10",
+      notes: "this is my history page",
+      exercises: "Push-up, Squat"
+    },
+    {
+      workout_id: 2,
+      workout_date: "2024-08-10",
+      notes: "this is my history page",
+      exercises: "Push-up, Squat"
+    },
+    {
+      workout_id: 3,
+      workout_date: "2024-08-10",
+      notes: "this is my history page",
+      exercises: "Push-up, Squat"
+    }
+  ]
   return (
     <div className="container">
       <h1>My Workout History</h1>
-
-      {sampleworkouts.map((workout)=>(
+      <div className='cards'>{sampleworkouts.map((workout) => (
         <Workout workout={workout} />
-      ))}
+      ))}</div>
+
     </div>
   );
 }
--- src/components/UserPage/workout.jsx
+++ src/components/UserPage/workout.jsx
@@ -1,21 +1,24 @@
 import Button from '@mui/material/Button';
-function Workout({workout}){
-    return <div>
+function Workout({ workout }) {
+    return <div className='workout'>
         <h2>{`${new Date(workout.workout_date).toDateString()} workout session`}</h2>
-        <h3>Exercises</h3>
-        <ul>
-            {workout.exercises.split(",").map((exercise)=>(
+        <div><h3>Exercises</h3>
+        <ul className='exercises'>
+            {workout.exercises.split(",").map((exercise) => (
                 <li>{exercise.trim()}</li>
             ))}
-        </ul>
-        <h3>My Notes</h3>
-        <p>{workout.notes}</p>
-        <div>
-        <Button variant="outlined">Edit</Button>
-        <Button variant="outlined">Delete</Button>
-        </div>
-        <div><Button variant="outlined">Reuse</Button></div>
+        </ul></div>
+        <div><h3>My Notes</h3>
+        <p>{workout.notes}</p></div>
         
+        <div className='buttons'>
+            <Button variant="outlined" color='info'>Edit</Button>
+            <Button variant="outlined">Reuse</Button>
+            <Button variant="outlined" color='error'>Delete</Button>
+
+        </div>
+
+
     </div>
 }
 
